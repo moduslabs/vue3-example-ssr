ce10c323df97c168da43695dae88b1b5b54cef5c
zhangzhuang15 <2592004894@qq.com>
2023-08-23 15:14:20 +0800

    fix #7, which contains 2 div element with same id; make the name of file more impressive; add activation of client side in ssr; add compiler config of client side in ssr;

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="main">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -17,7 +17,7 @@ export default {
 </script>
 
 <style>
-#app {
+#main {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
